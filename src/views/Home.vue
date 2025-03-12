2e02496757b352c52e2f033ddd1daef4c5643a22
mrleblanc101 <mrsebastienleblanc@icloud.com>
2025-03-11 21:10:45 -0400

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,41 @@
 import Drawer from '@/components/Drawer.vue'
 import JSONViewer from '@/components/JSONViewer.vue'
 import { useRegle } from '@regle/core'
-import { applyIf, minLength, required } from '@regle/rules'
+import { required } from '@regle/rules'
 import { ref } from 'vue'
 
-const condition = ref(true)
+const INITIAL_DATA = {
+  field1: '',
+  field2: '',
+};
+
+const form = ref({
+    tab: structuredClone(INITIAL_DATA),
+    tabs: Array.from({ length: 4 }, () => structuredClone(INITIAL_DATA)),
+});
 
 const { r$ } = useRegle(
-  { name: '' },
+  form,
   {
-    name: {
-      required: applyIf(condition, required),
-      minLength: applyIf(condition, minLength(3)),
+    tab: {
+      field1: { required },
+      field2: { required },
+    },
+    tabs: {
+        $each: {
+          field1: { required },
+          field2: { required },
+        },
     },
   },
 )
+
+const submit = async () => {
+    const { result: isValid } = await r$.$validate();
+    if (isValid) {
+      console.log('submit');
+    }
+};
 </script>
 
 <template>
@@ -23,11 +44,47 @@ const { r$ } = useRegle(
     <div class="mx-auto max-w-xl py-12 md:max-w-4xl">
       <h2 class="text-2xl mb-8">Regle reproduction</h2>
       <div class="flex flex-col">
-        <label>Your name</label>
-        <input class="border p-2 rounded" v-model="r$.$value.name" placeholder="Victor Regle" />
-        <ul class="text-red-400 text-sm mt-1" v-if="r$.$errors.name.length">
-          <li v-for="error of r$.$errors.name" :key="error">{{ error }}</li>
-        </ul>
+          <div>
+              <h2>Tab 1: {{ r$.$fields.tab.$error }}</h2>
+              <div class="grid grid-cols-2 items-start">
+                  <label class="flex flex-col rounded-lg bg-box p-8">
+                      field 1
+                      <input class="border p-2 rounded" v-model="form.tab.field1" />
+                      <ul class="text-red-400 text-sm mt-1" v-if="r$.$errors.tab.field1.length">
+                        <li v-for="error of r$.$errors.tab.field1" :key="error">{{ error }}</li>
+                      </ul>
+                  </label>
+                  <label class="flex flex-col rounded-lg bg-box p-8">
+                      field 2
+                      <input class="border p-2 rounded" v-model="form.tab.field2" />
+                      <ul class="text-red-400 text-sm mt-1" v-if="r$.$errors.tab.field2.length">
+                        <li v-for="error of r$.$errors.tab.field2" :key="error">{{ error }}</li>
+                      </ul>
+                  </label>
+              </div>
+          </div>
+          <div
+              v-for="(tab, i) in form.tabs"
+              :key="i"
+          >
+              <h2>Tab {{ i+2 }}: {{ r$.$fields.tabs.$each[i]?.$error }}</h2>
+              <div class="grid grid-cols-2 items-start">
+                <label class="flex flex-col rounded-lg bg-box p-8">
+                      field 1
+                      <input class="border p-2 rounded" v-model="tab.field1" />
+                      <ul class="text-red-400 text-sm mt-1" v-if="r$.$errors.tabs.$each[i].field1.length">
+                        <li v-for="error of r$.$errors.tabs.$each[i].field1" :key="error">{{ error }}</li>
+                      </ul>
+                  </label>
+                  <label class="flex flex-col rounded-lg bg-box p-8">
+                      field 2
+                      <input class="border p-2 rounded" v-model="tab.field2" />
+                      <ul class="text-red-400 text-sm mt-1" v-if="r$.$errors.tabs.$each[i].field2.length">
+                        <li v-for="error of r$.$errors.tabs.$each[i].field2" :key="error">{{ error }}</li>
+                      </ul>
+                  </label>
+              </div>
+          </div>
       </div>
       <Drawer>
         <JSONViewer :data="r$" />
